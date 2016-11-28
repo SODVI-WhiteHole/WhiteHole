6839673af30992ccff767bb3da6305b3287379ca
Luis <impel.up@gmail.com>
2016-11-28 00:31:09 -0600

    Programe la escena de Intro y la conecte con la recien creada escena de Historia Introductoria, ambas escenas fueron conectadas con la escena de Menú Principal, el cual de momento sigue vacio.

---
+++ Assets/Scripts/Historia_Intro/Historia_Intro.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Historia_Intro : MonoBehaviour {
+
+	/*Programado por José
+	Este código administra la escena Historia_Introductoria.
+	Corrige el texto segun el idioma elegido.
+	Manda al menuPrincipal una vez que se le da clic al botón de Skip.
+	*/
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+
+	public void irAMenuPrincipal(){
+		SceneManager.LoadScene("Menu_principal");
+	}
+
+	
+}
---
+++ Assets/Scripts/Intro/intro.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class intro : MonoBehaviour {
+
+	public bool siguiente=false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(siguiente){
+			SceneManager.LoadScene("Historia_introductoria");
+		}
+	}
+}
